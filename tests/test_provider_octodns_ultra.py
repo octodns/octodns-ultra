cd47c446e1755b016c06b4bd0e425e3258dde362
Ross McFarland <rwmcfa1@gmail.com>
2022-01-17 13:39:18 -0800

    Import skeleton

---
+++ tests/test_provider_octodns_ultra.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_ultra import UltraProvider
+
+
+class TestUltraProvider(TestCase):
+
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        UltraProvider
