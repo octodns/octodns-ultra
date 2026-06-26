6d8e117cfb5b72413b484aae0f8a0b197849301e
Ashley Tayles <ashley@tayles.uk>
2026-06-26 09:17:18 +0100

    v1.1.2 version bump

--- octodns_ultra/__init__.py
+++ octodns_ultra/__init__.py
@@ -14,7 +14,7 @@ from octodns.provider.base import BaseProvider
 from octodns.record import Create, Record, Update
 
 # TODO: remove __VERSION__ with the next major version release
-__version__ = __VERSION__ = '1.1.1'
+__version__ = __VERSION__ = '1.1.2'
 
 
 class UltraClientException(ProviderException):
